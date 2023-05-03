bdde6ee4ed300c354958ee8ebe090fe748fd34a8
Raiza Pagatpatan <rjimenezpagatpatan@gmail.com>
2023-05-03 11:49:19 +0800

    Yay

--- app/src/main/java/com/example/wildcats_hub/MainActivity.java
+++ app/src/main/java/com/example/wildcats_hub/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.wildcats_hub;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.actionBar);
+        setSupportActionBar(toolbar);
     }
 }
