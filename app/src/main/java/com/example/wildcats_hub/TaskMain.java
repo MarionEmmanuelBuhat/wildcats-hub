f22a398c4b8fc6c662d15c91f3e135418caf09fa
Peroxidize <marionemmanuelbuhat@gmail.com>
2023-05-11 20:13:41 +0800

    fix

--- app/src/main/java/com/example/wildcats_hub/TaskMain.java
+++ app/src/main/java/com/example/wildcats_hub/TaskMain.java
@@ -3,8 +3,16 @@ package com.example.wildcats_hub;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class TaskMain extends AppCompatActivity {
+
+    private ArrayList<String> items;
+    private ArrayAdapter<String> itemsAdapter;
+    private ListView listViewItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
