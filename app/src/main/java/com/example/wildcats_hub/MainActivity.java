10a96462ab89745dc2e2c07c133a7f4f7bd2116e
Peroxidize <marionemmanuelbuhat@gmail.com>
2023-05-11 19:53:37 +0800

    added event listeners

--- app/src/main/java/com/example/wildcats_hub/MainActivity.java
+++ app/src/main/java/com/example/wildcats_hub/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.wildcats_hub;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
-import androidx.appcompat.widget.Toolbar;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button login, register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        login = findViewById(R.id.loginButton);
+        register = findViewById(R.id.createAccButton);
+
+
+        login.setOnClickListener(view -> {
+            startActivity(new Intent(this, TaskMain.class));
+        });
+
+        register.setOnClickListener(view -> {
+            startActivity(new Intent(this, Register.class));
+        });
     }
+
+
 }
--- app/src/main/java/com/example/wildcats_hub/Register.java
+++ app/src/main/java/com/example/wildcats_hub/Register.java
@@ -2,13 +2,22 @@ package com.example.wildcats_hub;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Register extends AppCompatActivity {
+
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        login = findViewById(R.id.homeButton);
+
+        login.setOnClickListener(view -> {
+            startActivity(new Intent(this, MainActivity.class));
+        });
     }
 }
