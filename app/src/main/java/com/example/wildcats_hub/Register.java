117eccbefabc6cd1d76e2d6d72eadec933a92718
Raiza Pagatpatan <rjimenezpagatpatan@gmail.com>
2023-05-11 20:30:12 +0800

    Added Register Intent

--- app/src/main/java/com/example/wildcats_hub/Register.java
+++ app/src/main/java/com/example/wildcats_hub/Register.java
@@ -4,20 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class Register extends AppCompatActivity {
 
     Button login;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Button register = findViewById(R.id.buttonRegister);
+
         login = findViewById(R.id.homeButton);
 
         login.setOnClickListener(view -> {
             startActivity(new Intent(this, MainActivity.class));
         });
+
+        register.setOnClickListener(view -> goLogin());
+    }
+
+    public void goLogin(){
+        Intent createLogin = new Intent(this, MainActivity.class);
+        startActivity(createLogin);
     }
 }
