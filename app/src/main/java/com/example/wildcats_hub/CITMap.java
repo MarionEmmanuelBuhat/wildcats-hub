844ced69360e11562c789bef887ffbba6efcc4c0
Raiza Pagatpatan <rjimenezpagatpatan@gmail.com>
2023-05-23 21:10:56 +0800

    Updated Maps

--- app/src/main/java/com/example/wildcats_hub/CITMap.java
+++ app/src/main/java/com/example/wildcats_hub/CITMap.java
@@ -9,24 +9,21 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageButton;
 
-public class CITMap extends AppCompatActivity {
 
+
+public class CITMap extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_citmap);
+
+
         ImageButton CITMapOpenBtn = findViewById(R.id.btnopenMaps);
-
         CITMapOpenBtn.setOnClickListener(view -> openCITGMaps());
-
-
-
-
     }
 
-
-    public void openCITGMaps(){
+    public void openCITGMaps() {
         String url = "https://maps.app.goo.gl/qcYPuLBSBAKM7iSr6";
         Intent openMaps = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
         startActivity(openMaps);
